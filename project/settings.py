ec2808d51cdf8f01842a478edc1b0e1632f8f09d
Desha <mostafamohamed92895@gmail.com>
2026-02-27 03:52:27 +0200

    add my main  apps

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop',
+    'accounts',
+    'blog',
+    'contact',
+    'about',
 ]
 
 MIDDLEWARE = [
