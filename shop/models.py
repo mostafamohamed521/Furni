9c7640d03bb150979e0df9dd81b13d86e35241bc
Desha <mostafamohamed92895@gmail.com>
2026-02-27 04:28:31 +0200

    add some feom code in models shop

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class shop(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='shop/')
+    price = models.IntegerField(default=0)
+
+     
+    def __str__(self):
+        return self.name
+
+class statue(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
+
+class addcart(models.Model):
+    
