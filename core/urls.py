b2a24ba235a65f94c3e6aca6ae8d6c29a704dd42
Desha <mostafamohamed92895@gmail.com>
2026-08-12 02:46:49 +0300

    Configure URL routes for core website pages

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
+    path('faq/', views.faq, name='faq'),
+    path('terms/', views.terms, name='terms'),
+    path('privacy/', views.privacy, name='privacy'),
+]
