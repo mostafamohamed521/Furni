639938f929c2982a96cee88fdab668c184a8a2b9
Desha <mostafamohamed92895@gmail.com>
2026-08-13 01:54:40 +0300

    Update account administration and security management

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, OTPCode
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'phone', 'city', 'country')
     search_fields = ('user__username', 'user__email', 'phone')
+
+
+@admin.register(OTPCode)
+class OTPCodeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'purpose', 'code', 'is_used', 'created_at')
+    list_filter = ('purpose', 'is_used')
+    search_fields = ('user__username', 'user__email', 'code')
+    readonly_fields = ('user', 'code', 'purpose', 'created_at')
+
+    def has_add_permission(self, request):
+        return False
