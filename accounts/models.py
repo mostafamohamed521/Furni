aebf7ed54d254938a9dc9002b476744b79d9c936
Desha <mostafamohamed92895@gmail.com>
2026-08-13 01:54:45 +0300

    Update user account and profile models

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
+from datetime import timedelta
 
 
 class Profile(models.Model):
@@ -23,3 +25,30 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
     else:
         Profile.objects.get_or_create(user=instance)
+
+
+class OTPCode(models.Model):
+    """One-time password used for two-factor login and registration email verification."""
+    PURPOSE_CHOICES = [
+        ('register', 'Registration'),
+        ('login', 'Login'),
+    ]
+    VALIDITY_MINUTES = 10
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='otp_codes')
+    code = models.CharField(max_length=6)
+    purpose = models.CharField(max_length=10, choices=PURPOSE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_used = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.get_purpose_display()} OTP for {self.user.username}'
+
+    def is_expired(self):
+        return timezone.now() > self.created_at + timedelta(minutes=self.VALIDITY_MINUTES)
+
+    def is_valid(self):
+        return not self.is_used and not self.is_expired()
