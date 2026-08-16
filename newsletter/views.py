d35b91aa77c8194cd83ea5aa14f409dbbc549e76
Desha <mostafamohamed92895@gmail.com>
2026-08-16 10:17:03 +0300

    newsletter/views.py: Update newsletter views

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.views.decorators.http import require_POST
+
 from .forms import SubscriberForm
+from .models import Subscriber
+from .utils import verify_unsubscribe_token
 
 
 @require_POST
@@ -9,10 +12,19 @@ def subscribe(request):
     form = SubscriberForm(request.POST)
     if form.is_valid():
         form.save()
-        messages.success(request, 'Thanks for subscribing to our newsletter!')
+        messages.success(request, 'Thanks for subscribing to our newsletter! You can unsubscribe at any time using the link in any email we send you.')
     else:
         if 'email' in form.errors:
             messages.error(request, 'This email is already subscribed or invalid.')
         else:
             messages.error(request, 'Something went wrong. Please try again.')
     return redirect(request.META.get('HTTP_REFERER', '/'))
+
+
+def unsubscribe(request, token):
+    email = verify_unsubscribe_token(token)
+    if not email:
+        return render(request, 'newsletter/unsubscribe.html', {'success': False})
+
+    updated = Subscriber.objects.filter(email__iexact=email).update(is_active=False)
+    return render(request, 'newsletter/unsubscribe.html', {'success': bool(updated), 'email': email})
